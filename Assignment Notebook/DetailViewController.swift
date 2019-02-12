70e48efa145d339422619f001d9cfa2a4b57e375
zamin ahmed <zahmed@zamins-macbook-air.local>
2019-02-12 11:21:05 -0600

    Connected UI elements to storyboard

--- Assignment Notebook/DetailViewController.swift
+++ Assignment Notebook/DetailViewController.swift
@@ -9,32 +9,59 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureView()
-    }
-
-    var detailItem: NSDate? {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var subjectTextField: UITextField!
+    
+    @IBOutlet weak var dueDateTextField: UITextField!
+    
+    @IBOutlet weak var DescriptionTextField: UITextView!
+    
+    var detailItem: Assignments? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
+    
+    func configureView(){
+        if let  assignment = self.detailItem {
+            if titleTextField != nil {
+                titleTextField.text = assignment.title
+                subjectTextField.text = assignment.subject
+                dueDateTextField.text = String(assignment.DueDate)
+                DescriptionTextField.text = assignment.Description
+                }
+          
+            }
+     func  viewWillDisappear(){
+            if let assignment = self.detailItem{
+                assignment.title = titleTextField.text!
+                assignment.subject = subjectTextField.text!
+                assignment.DueDate = Int(dueDateTextField.text!)!
+                assignment.Description = DescriptionTextField.text
+                
+                
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+    
+  
+    
 }
 
