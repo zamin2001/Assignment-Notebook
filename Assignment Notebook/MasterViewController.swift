b67505be69a01ccac88aef8e04db35d8b23700ba
zamin ahmed <zahmed@zamins-macbook-air.local>
2019-02-13 11:15:09 -0600

    Added Alert View for assignments

--- Assignment Notebook/MasterViewController.swift
+++ Assignment Notebook/MasterViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
-
+    var assignments = [Assignments]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
-
+        
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
         navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
@@ -26,21 +26,62 @@ class MasterViewController: UITableViewController {
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
+    
     @objc
     func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+        let alert = UIAlertController(title: "Add Assignment", message:nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Title"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Subject"
+        }
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Description"
+        }
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Due Date"
+            textField.keyboardType = .numberPad
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style:.cancel, handler:nil)
+        alert.addAction(cancelAction)
+        
+        let insertAction = UIAlertAction(title:"Add", style:.default) { (action) in
+            let titleTextField = alert.textFields![0] as UITextField
+            let subjectTextField = alert.textFields![1] as UITextField
+            let duedateTextField = alert.textFields![2] as UITextField
+            
+            if let DueDate = Int(duedateTextField.text!){
+                let assignment = Assignments(title:titleTextField.text!,subject:subjectTextField.text!,DueDate:DueDate, Description:self.description)
+                
+                self.assignments.append(assignment)
+                self.tableView.reloadData()
+                
+                
+                
+                
+            }
+        
+        }
+        
+        
+        alert.addAction(insertAction)
+        present(alert,animated: true, completion: nil)
+        
     }
-
+    
+    
+    
     // MARK: - Segues
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
@@ -52,30 +93,30 @@ class MasterViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         let object = objects[indexPath.row] as! NSDate
         cell.textLabel!.text = object.description
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             objects.remove(at: indexPath.row)
@@ -84,7 +125,7 @@ class MasterViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
-
+    
+    
 }
 
